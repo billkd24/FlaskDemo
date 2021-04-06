e5c554b7541ac603501eacb653ef233f5f76fec4
billkd24 <nyerere.akide@strathmore.edu>
2021-04-06 11:50:52 +0300

    flask2

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+    return render_templates('index.html')
+
+@app.route("/home")
+ def home():
+     return render_templates('home.html') 
+
+@app.route("/courses")
+def greet():
+    return "Have a Good Day!"
